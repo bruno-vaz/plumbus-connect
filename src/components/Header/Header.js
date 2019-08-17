1447caf68f1db8bae55cc500664acb2296fa4ce8
bruno-vaz <brunovaz.frontend@gmail.com>
2019-08-17 13:31:31 -0300

    :sparkles: Added button to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,16 +4,22 @@ import { Link } from "gatsby"
 import styles from "./Header.module.scss"
 
 import Container from "components/Container"
+import Button from "components/Button"
 
 import PlumbusConnectLogo from "src/icons/plumbus-connect-logo.svg"
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <Container>
+      <Container
+        className={styles.container}
+      >
         <Link to="/" className={styles.logo}>
           <PlumbusConnectLogo/>
         </Link>
+        <Button
+          label="Be the first"
+        />
       </Container>
     </header>
   )
