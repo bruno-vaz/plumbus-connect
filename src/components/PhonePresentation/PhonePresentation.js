ccc96c4fc1923e1a2e5135a869c416e97b7db036
bruno-vaz <brunovaz.frontend@gmail.com>
2019-08-17 18:09:19 -0300

    :sparkles: Hero illustrations

--- src/components/PhonePresentation/PhonePresentation.js
+++ src/components/PhonePresentation/PhonePresentation.js
@@ -3,11 +3,23 @@ import React from "react"
 import styles from "./PhonePresentation.module.scss"
 
 import PhoneImg from "src/images/phone.png"
-import FloobIcon from "src/icons/floob.svg"
-import GrumboIcon from "src/icons/grumbo.svg"
+import Floob from "components/illustrations/Floob"
+import Grumbo from "components/illustrations/Grumbo"
 
 const PhonePresentation = () => (
   <section className={styles.container}>
+    <div className={styles.illustrations}>
+      <div className={styles.grumbo}>
+        <Grumbo/>
+      </div>
+      <div className={styles.grumbo}>
+        <Grumbo/>
+      </div>
+      <div className={styles.grumbo}>
+        <Grumbo/>
+      </div>
+      <Floob/>
+    </div>
     <div className={styles.phone}>
       <img src={PhoneImg} alt="Smartphone" />
     </div>
---
+++ src/components/illustrations/Floob/Floob.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+import styles from "./Floob.module.scss"
+
+const Floob = () => (
+  <div className={styles.floob}>
+    <svg width="693" height="575" viewBox="0 0 693 575" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="586" cy="407" r="107"/>
+      <circle cx="469" cy="407" r="107"/>
+      <circle cx="356.5" cy="235.5" r="166.5"/>
+      <circle cx="493" cy="300" r="107"/>
+      <circle cx="379" cy="468" r="107"/>
+      <circle cx="232" cy="434" r="107"/>
+      <circle cx="107" cy="321" r="107"/>
+      <circle cx="197" cy="163" r="107"/>
+      <circle cx="469" cy="135" r="107"/>
+      <circle cx="250" cy="107" r="107"/>
+    </svg>
+  </div>
+)
+
+export default Floob
