f498db2fb29a4df451559dc5681f7dddfe91727f
bruno-vaz <brunovaz.frontend@gmail.com>
2019-08-18 20:41:48 -0300

    :mag: Helmet

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,17 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
 import "./Layout.scss"
 
 const Layout = ({ children }) => {
   return (
-    <main>{children}</main>
+    <main>
+      <Helmet>
+        <title>Plumbus Connect</title>
+        <meta name="theme-color" content="#fa9a9c"/>
+      </Helmet>
+      {children}
+    </main>
   )
 }
 
